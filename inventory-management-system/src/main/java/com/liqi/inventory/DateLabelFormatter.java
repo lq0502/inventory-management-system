7e8e936c82515354c6b43b3446f368938d3320a2
lq0502 <148927487+lq0502@users.noreply.github.com>
2025-06-20 10:12:28 +0900

    Update DateLabelFormatter.java

--- inventory-management-system/src/main/java/com/liqi/inventory/DateLabelFormatter.java
+++ inventory-management-system/src/main/java/com/liqi/inventory/DateLabelFormatter.java
@@ -8,15 +8,17 @@ public class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
     private static final String DATE_PATTERN = "yyyy-MM-dd";
     private static final SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_PATTERN);
     @Override
+    //文字列をDateに変換
     public Object stringToValue(String text) throws ParseException {
         return dateFormatter.parse(text);
     }
     @Override
+    //Dateを表示用の文字列に変換
     public String valueToString(Object value) {
         if (value != null) {
             Calendar cal = (Calendar) value;
             return dateFormatter.format(cal.getTime());
         }
-        return "";
+        return "";//null空文字列
     }
 }
