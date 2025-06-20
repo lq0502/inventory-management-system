afbd8ed79409aa6a84ef0aedeb3dbee11c93e4ec
lq0502 <148927487+lq0502@users.noreply.github.com>
2025-06-20 10:08:47 +0900

    Update DatabaseConnection.java

--- inventory-management-system/src/main/java/com/liqi/inventory/DatabaseConnection.java
+++ inventory-management-system/src/main/java/com/liqi/inventory/DatabaseConnection.java
@@ -1,20 +1,26 @@
 package com.liqi.inventory;
+// JDBC必要なクラス
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//データベースに接続するメソッド
 public class DatabaseConnection {
     private static final String DB_URL = "jdbc:sqlite:" + System.getProperty("user.dir") + "/inventory.db";
     public static Connection connect() {
-        Connection conn = null;
+        Connection conn = null;//変数を初期化
         try {
+            //SQLiteに接続
             conn = DriverManager.getConnection(DB_URL);
-            System.out.println("数据库连接成功！");
+            System.out.println("成功");
         } catch (SQLException e) {
-            System.out.println("数据库连接失败: " + e.getMessage());
+            //デバッグ
+            System.out.println("失败: " + e.getMessage());
         }
         return conn;
     }
+
+    //テスト用......
     public static void main(String[] args) {
         connect();
     }
